4a11ee7d2ba0b63a40d5a9c076333deb8c49f8b4
rogger <rvaldiviacarnica@gmail.com>
2026-05-24 15:20:48 -0500

    Refactor main.py to initialize FastAPI and include routers for deportes and candidato

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from app.core.deportes_rest import app
-from app.core.candidato_rest import app
+from fastapi import FastAPI
+
+from app.core.deportes_rest import app as deportes_app
+from app.core.candidato_rest import app as candidato_app
 import uvicorn
+
+
+app = FastAPI()
+app.include_router(deportes_app.router)
+app.include_router(candidato_app.router)
 
 
 def main():
     print("🚀 API iniciada en http://127.0.0.1:8000")
 
     uvicorn.run(
+        app,
         host="127.0.0.1",
         port=8000,
         reload=True
